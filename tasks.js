78a8bb6d4d3163d2faae93da2dfb7ba80e5c6b2e
Shodlik Shomuratov <shodlikcoder@gmail.com>
2024-07-30 16:55:54 +0500

    new tasks

--- tasks.js
+++ tasks.js
@@ -161,3 +161,138 @@
 // asd();
 
 /* ------------------------------------ */
+
+// function firstNonRepeatingLetter(s) {
+//   return (
+//     s.split("").find((letter, index) => {
+//       let str = s.slice(0, index) + s.slice(index + 1);
+
+//       return !str.toLowerCase().includes(letter.toLowerCase());
+//     }) || ""
+//   );
+// }
+
+// console.log(firstNonRepeatingLetter("sTreSS"));
+
+/* ------------------------------------ */
+
+// function levenshteinDistance(a, b) {
+//   const matrix = [];
+//   for (let i = 0; i <= a.length; i++) {
+//     matrix[i] = [];
+//     for (let j = 0; j <= b.length; j++) {
+//       matrix[i][j] = 0;
+//     }
+//   }
+
+//   for (let i = 0; i <= a.length; i++) {
+//     matrix[i][0] = i;
+//   }
+//   for (let j = 0; j <= b.length; j++) {
+//     matrix[0][j] = j;
+//   }
+
+//   for (let i = 1; i <= a.length; i++) {
+//     for (let j = 1; j <= b.length; j++) {
+//       const cost = a[i - 1] === b[j - 1] ? 0 : 1;
+//       matrix[i][j] = Math.min(
+//         matrix[i - 1][j] + 1,
+//         matrix[i][j - 1] + 1,
+//         matrix[i - 1][j - 1] + cost
+//       );
+//     }
+//   }
+
+//   return matrix[a.length][b.length];
+// }
+
+// function Dictionary(words) {
+//   this.words = words;
+// }
+
+// Dictionary.prototype.findMostSimilar = function (term) {
+//   const distances = [];
+
+//   this.words.forEach((word, index) => {
+//     distances[index] = levenshteinDistance(word, term);
+//   });
+
+//   return this.word[distances.indexOf(Math.min(...distances))];
+// };
+
+// const fruits = new Dictionary([
+//   "cherry",
+//   "pineapple",
+//   "melon",
+//   "strawberry",
+//   "raspberry",
+// ]);
+// console.log(fruits.findMostSimilar("strawbery")); // must return "strawberry"
+// console.log(fruits.findMostSimilar("berry")); // must return "cherry"
+
+/* ------------------------------------ */
+
+// let isInteger = (number) => {
+//   const split = String(number).split(".");
+//   return split.length === 1;
+// };
+
+// const round = function (number) {
+//   const split = String(number).split(".");
+//   if (isInteger(number)) return number;
+
+//   const decimal = Number(split[1][0]);
+//   const int = Number(split[0]);
+
+//   return decimal >= 5 ? Number(int) + 1 : Number(int);
+// };
+
+// const ceil = function (number) {
+//   if (isInteger(number)) return number;
+//   return Number(String(number).split(".")[0]) + 1;
+// };
+
+// const floor = function (number) {
+//   if (isInteger(number)) return number;
+//   return Number(String(number).split(".")[0]);
+// };
+
+// console.log(round(1));
+// console.log(ceil(0));
+// console.log(floor(1));
+
+/* ------------------------------------ */
+
+// function ipsBetween(start, end) {
+//   const [x1, y1, z1, t1] = start.split(".").map((el) => +el);
+//   const [x2, y2, z2, t2] = end.split(".").map((el) => +el);
+
+//   return (
+//     (x2 - x1) * 2 ** 24 + (y2 - y1) * 2 ** 16 + (z2 - z1) * 2 ** 8 + (t2 - t1)
+//   );
+// }
+
+// console.log(ipsBetween("50.0.0.0", "50.1.1.1"));
+
+/* ------------------------------------ */
+
+const isShuffled = (a, b) => {
+  return (
+    String(a).split("").sort().join("") === String(b).split("").sort().join("")
+  );
+};
+
+function nextSmaller(n) {
+  let res = n - 1;
+
+  if (n === +String(n).split("").sort().join("")) return -1;
+
+  while (!isShuffled(res, n)) {
+    if (String(res).length < String(n).length) return -1;
+    --res;
+  }
+
+  return res || -1;
+}
+
+console.log(nextSmaller(9199999));
